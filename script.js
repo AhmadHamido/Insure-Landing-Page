9b41d03cbeca351dec184ee821091c1bf8ce1a3e
Ahmad Hamido <letraingrand1@gmail.com>
2023-01-23 09:58:39 +0100

    Modified the CSS

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 if (document.body.style.width >= "811px") {
     console.log("The screen is big")
     // document.getElementById('nav').innerHTML = `
-    //     <ul>
-    //         <li><a href="#">HOW WE WORK</a></li>
-    //         <li><a href="#">BLOG</a></li>
-    //         <li><a href="#">ACCOUNT</a></li>
-    //         <li><a href="#">VIEW PLANS</a></li>
-    //     </ul>
+    //        <a href="#">HOW WE WORK</a>
+    //        <a href="#">BLOG</a>
+    //        <a href="#">ACCOUNT</a>
+    //        <a href="#">VIEW PLANS</a>
     // `
 }
