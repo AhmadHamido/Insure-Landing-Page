935c704a3f700b5182ef6d7e76bc0642cd656061
Ahmad Hamido <letraingrand1@gmail.com>
2023-01-23 09:28:14 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+if (document.body.style.width >= "811px") {
+    console.log("The screen is big")
+    // document.getElementById('nav').innerHTML = `
+    //     <ul>
+    //         <li><a href="#">HOW WE WORK</a></li>
+    //         <li><a href="#">BLOG</a></li>
+    //         <li><a href="#">ACCOUNT</a></li>
+    //         <li><a href="#">VIEW PLANS</a></li>
+    //     </ul>
+    // `
+}
